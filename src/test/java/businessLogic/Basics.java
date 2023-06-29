752f9bd3176db1112fb6f710a1660ccd36934525
Tang0charli3 <darshanynvfx@gmail.com>
2023-06-29 22:16:50 +0530

    29/06/2023

---
+++ src/test/java/businessLogic/Basics.java
@@ -0,0 +1,11 @@
+package businessLogic;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+public class Basics {
+public static void main(String[] args) {
+	RestAssured.baseURI="https://rahulshettyacademy.com";
+//	given().
+}
+}
