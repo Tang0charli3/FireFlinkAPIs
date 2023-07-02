39a83a3158bd5792aa9da0b5d58d540dadb157f6
Tang0charli3 <darshanynvfx@gmail.com>
2023-07-02 22:37:14 +0530

    07/07/2023

--- src/test/java/businessLogic/Basics.java
+++ src/test/java/businessLogic/Basics.java
@@ -1,11 +1,40 @@
 package businessLogic;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class Basics {
 public static void main(String[] args) {
 	RestAssured.baseURI="https://rahulshettyacademy.com";
-//	given().
+	String response=given().queryParam("key", "qaclick123").header("Content-Type","application/json")
+	.body("{\r\n"
+			+ "  \"location\": {\r\n"
+			+ "    \"lat\": -38.383494,\r\n"
+			+ "    \"lng\": 33.427362\r\n"
+			+ "  },\r\n"
+			+ "  \"accuracy\": 50,\r\n"
+			+ "  \"name\": \"Frontline house\",\r\n"
+			+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
+			+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
+			+ "  \"types\": [\r\n"
+			+ "    \"shoe park\",\r\n"
+			+ "    \"shop\"\r\n"
+			+ "  ],\r\n"
+			+ "  \"website\": \"http://google.com\",\r\n"
+			+ "  \"language\": \"French-IN\"\r\n"
+			+ "}").when().post("maps/api/place/add/json")
+	.then().assertThat().statusCode(200).extract().response().asString();
+	System.out.println(response);
+	JsonPath js =new JsonPath(response);//for parsing
+	String place_id=js.get("place_id");
+	System.out.println(place_id);
+//	if(scope.equals("APP")) {
+//		System.out.println("Passed");
+//	}
+	
+	
 }
 }
